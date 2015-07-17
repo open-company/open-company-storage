967e9238ad73b2152697910e5bc35f5fff010eff
Sean Johnson <sean@snootymonkey.com>
2015-07-17 07:09:29 -0400

    Script to install RethinkDB on Travis CI.

---
+++ opt/install-rethinkdb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# -e exit as soon as there is an error
+# -v verbosely echo script contents
+set -ev
+
+# Add the RethinkDB repository and install the RethinkDB package
+sudo add-apt-repository ppa:rethinkdb/ppa
+sudo apt-get update
+sudo apt-get install rethinkdb
