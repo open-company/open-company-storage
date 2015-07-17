88f3e589d5f0b6ebb1c808df637352fd579a9e11
Sean Johnson <sean@snootymonkey.com>
2015-07-17 08:20:52 -0400

    Add non-prompting confirmation to add-apt-repository for Travis.

--- opt/install-rethinkdb.sh
+++ opt/install-rethinkdb.sh
@@ -5,6 +5,6 @@
 set -ev
 
 # Add the RethinkDB repository and install the RethinkDB package
-sudo add-apt-repository ppa:rethinkdb/ppa
+sudo add-apt-repository ppa:rethinkdb/ppa -y
 sudo apt-get update
 sudo apt-get install rethinkdb
