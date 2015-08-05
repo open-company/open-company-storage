f27ff569ec7d982bd54c09c42613aeea1ec8c59e
Sean Johnson <sean@snootymonkey.com>
2015-08-05 07:09:03 -0400

    Update midje dependencies.

--- project.clj
+++ project.clj
@@ -46,11 +46,11 @@
         :hot-reload false
       }
       :dependencies [
-        [midje "1.7.0"] ; Example-based testing https://github.com/marick/Midje
+        [midje "1.8-alpha1"] ; Example-based testing https://github.com/marick/Midje
         [ring-mock "0.1.5"] ; Test Ring requests https://github.com/weavejester/ring-mock
       ]
       :plugins [
-        [lein-midje "3.1.3"] ; Example-based testing https://github.com/marick/lein-midje
+        [lein-midje "3.2-RC1"] ; Example-based testing https://github.com/marick/lein-midje
         [jonase/eastwood "0.2.1"] ; Clojure linter https://github.com/jonase/eastwood
       ]
     }
