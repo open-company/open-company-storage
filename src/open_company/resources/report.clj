7f3afc4239043e2ffe59ff8f0bf42db1a6b57840
Sean Johnson <sean@snootymonkey.com>
2015-08-13 07:14:36 -0400

    Allow report/get-report to still take just a report-key as an argument as that arity/1 version is used in a guard expression.

--- src/open_company/resources/report.clj
+++ src/open_company/resources/report.clj
@@ -1,5 +1,6 @@
 (ns open-company.resources.report
-  (:require [defun :refer (defun)]
+  (:require [clojure.string :as s]
+            [defun :refer (defun)]
             [rethinkdb.query :as r]
             [open-company.config :as c]
             [open-company.resources.common :as common]
@@ -53,10 +54,11 @@
 (defn get-report
   "Given the ticker symbol of the company and the year and period of the report,
   or the primary key, retrieve it from the database, or return nil if it doesn't exist."
-  ([ticker year period] (get-report ticker (key-for ticker year period)))
+  ([report-key] (get-report (first (s/split report-key #"-")) report-key))
   ([ticker report-key]
     (when (company/get-company ticker)
-      (common/read-resource table-name report-key))))
+      (common/read-resource table-name report-key)))
+  ([ticker year period] (get-report ticker (key-for ticker year period))))
 
 (defun create-report
   "Given the report property map, create the report returning the property map for the resource or `false`.
