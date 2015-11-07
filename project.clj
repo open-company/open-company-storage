1da3719c468dd9d26c946deba97ab26c1698481c
Sean Johnson <sean@snootymonkey.com>
2015-11-07 06:28:09 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -50,7 +50,7 @@
         :open-company-auth-passphrase "this_is_a_qa_secret" ; JWT secret
       }
       :dependencies [
-        [midje "1.8.1"] ; Example-based testing https://github.com/marick/Midje
+        [midje "1.8.2"] ; Example-based testing https://github.com/marick/Midje
         [ring-mock "0.1.5"] ; Test Ring requests https://github.com/weavejester/ring-mock
       ]
       :plugins [
