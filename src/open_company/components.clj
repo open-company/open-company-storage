8cb812d20efc1b28b34eba3d3a8451c516b94172
Martin Klepsch <martinklepsch@googlemail.com>
2016-03-24 15:31:49 +0100

    ensure httpkit is only stopped if started + oc-system constructor

--- src/open_company/components.clj
+++ src/open_company/components.clj
@@ -8,34 +8,46 @@
   component/Lifecycle
   (start [component]
     (let [handler (get-in component [:handler :handler] handler)
-          server (httpkit/run-server handler options)]
-      (timbre/info "Starting HTTPKit server")
+          server  (httpkit/run-server handler options)]
       (assoc component :server server)))
   (stop [component]
-    (when server
-      (server)
-      component)))
+    ;; (prn component)
+    (if-not server
+      component
+      (do
+        (server)
+        (dissoc component :server)))))
 
 (defrecord RethinkPool [size regenerate-interval pool]
   component/Lifecycle
   (start [component]
-    (timbre/info "Starting RethinkDB connection pool")
     (let [pool (pool/fixed-pool pool/init-conn pool/close-conn
                                 {:size size :regenerate-interval 15})]
       (assoc component :pool pool)))
   (stop [component]
-    (when pool
-      (do 
-        (timbre/info "Stopping RethinkDB connection pool")
+    (if pool
+      (do
         (pool/shutdown-pool! pool)
         (dissoc component :pool))
       component)))
 
 (defrecord Handler [handler-fn]
+  component/Lifecycle
   (start [component]
     (assoc component :handler (handler-fn component)))
   (stop [component]
     (dissoc component :handler)))
 
-(def pool
-  (map->RethinkPool {:size 5 :regenerate-interval 10}))
+;; (def pool
+;;   (map->RethinkPool {:size 5 :regenerate-interval 10}))
+
+(defn oc-system [opts]
+  (let [{:keys [host port handler-fn]} opts]
+    (component/system-map
+     :db-pool (map->RethinkPool {:size 5 :regenerate-interval 5})
+     :handler (component/using
+               (map->Handler {:handler-fn handler-fn})
+               [:db-pool])
+     :server  (component/using
+               (map->HttpKit {:options {:port port}})
+               [:handler]))))
