50a9d67c1326675b222398b68b8adbb839105bfd
Sean Johnson <sean@snootymonkey.com>
2016-04-19 06:38:27 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -24,12 +24,12 @@
     [http-kit "2.2.0-alpha1"] ; Web server http://http-kit.org/
     [compojure "1.5.0"] ; Web routing https://github.com/weavejester/compojure
     [liberator "0.14.1"] ; WebMachine (REST API server) port to Clojure https://github.com/clojure-liberator/liberator
-    [com.apa512/rethinkdb "0.15.12"] ; RethinkDB client for Clojure https://github.com/apa512/clj-rethinkdb
+    [com.apa512/rethinkdb "0.15.13"] ; RethinkDB client for Clojure https://github.com/apa512/clj-rethinkdb
     [prismatic/schema "1.1.0"] ; Data validation https://github.com/Prismatic/schema
     [environ "1.0.2"] ; Environment settings from different sources https://github.com/weavejester/environ
     [com.taoensso/timbre "4.3.1"] ; Logging https://github.com/ptaoussanis/timbre
     [raven-clj "1.3.1"] ; Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
-    [clj-http "2.1.0"] ; HTTP client https://github.com/dakrone/clj-http
+    [clj-http "3.0.1"] ; HTTP client https://github.com/dakrone/clj-http
     [org.clojure/tools.cli "0.3.3"] ; Command-line parsing https://github.com/clojure/tools.cli
     [clj-jwt "0.1.1"] ; Library for JSON Web Token (JWT) https://github.com/liquidz/clj-jwt
     [medley "0.7.4"] ; Utility functions https://github.com/weavejester/medley
