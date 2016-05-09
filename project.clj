0fb57aed2438fda51dab16a5bbef6836063c2256
Martin Klepsch <martinklepsch@googlemail.com>
2016-05-09 15:36:29 +0200

    update raven-clj

--- project.clj
+++ project.clj
@@ -28,8 +28,7 @@
     [prismatic/schema "1.1.1"] ; Data validation https://github.com/Prismatic/schema
     [environ "1.0.3"] ; Environment settings from different sources https://github.com/weavejester/environ
     [com.taoensso/timbre "4.4.0-alpha1"] ; Logging https://github.com/ptaoussanis/timbre
-    [raven-clj "1.3.1"] ; Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
-    [clj-http "3.0.1"] ; HTTP client https://github.com/dakrone/clj-http
+    [raven-clj "1.3.2"] ; Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
     [org.clojure/tools.cli "0.3.5"] ; Command-line parsing https://github.com/clojure/tools.cli
     [clj-jwt "0.1.1"] ; Library for JSON Web Token (JWT) https://github.com/liquidz/clj-jwt
     [medley "0.8.1"] ; Utility functions https://github.com/weavejester/medley
