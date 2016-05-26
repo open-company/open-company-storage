0bfc9033b9dabdbd320b3b8d9f65c7c50fdd4d14
Martin Klepsch <martinklepsch@googlemail.com>
2016-05-26 17:08:09 +0200

    log error if JWToken is expired

--- src/open_company/lib/jwt.clj
+++ src/open_company/lib/jwt.clj
@@ -1,20 +1,28 @@
 (ns open-company.lib.jwt
-  (:require [clj-jwt.core :refer :all]
+  (:require [clj-jwt.core :as jwt]
+            [taoensso.timbre :as timbre]
+            [clj-time.core :as t]
+            [clj-time.coerce :as tc]
             [open-company.config :as config]))
+
+(defn expired?
+  [jwt-claims]
+  (t/after? (t/now) (tc/from-long (:expire jwt-claims))))
 
 (defn check-token
   "Verify a JSON Web Token"
   [token]
   (try
-    (do
-      (-> token
-        str->jwt
-        (verify config/passphrase))
-      true)
+    (let [jwt (-> token jwt/str->jwt)]
+      (when (expired? (:claims jwt))
+        (timbre/error "Request made with expired JWToken" (:claims jwt)))
+      (do
+        (-> token jwt/str->jwt (jwt/verify config/passphrase))
+        true))
     (catch Exception e
       false)))
 
 (defn decode
   "Decode a JSON Web Token"
   [token]
-  (str->jwt token))
+  (jwt/str->jwt token))
