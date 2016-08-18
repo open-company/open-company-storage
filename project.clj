c4e76aaaaed3aa9a82d99621be7ccca125bfac04
Sean Johnson <sean@snootymonkey.com>
2016-08-18 07:42:28 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -18,8 +18,8 @@
     [org.clojure/core.async "0.2.385"] ; Dependency of core.match and RethinkDB https://github.com/clojure/core.async
     [defun "0.3.0-alapha"] ; Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     [lockedon/if-let "0.1.0"] ; More than one binding for if/when macros https://github.com/LockedOn/if-let
-    [ring/ring-devel "1.6.0-beta4"] ; Web application library https://github.com/ring-clojure/ring
-    [ring/ring-core "1.6.0-beta4"] ; Web application library https://github.com/ring-clojure/ring
+    [ring/ring-devel "1.6.0-beta5"] ; Web application library https://github.com/ring-clojure/ring
+    [ring/ring-core "1.6.0-beta5"] ; Web application library https://github.com/ring-clojure/ring
     [jumblerg/ring.middleware.cors "1.0.1"] ; CORS library https://github.com/jumblerg/ring.middleware.cors
     [http-kit "2.2.0"] ; Web server http://http-kit.org/
     [compojure "1.6.0-beta1"] ; Web routing https://github.com/weavejester/compojure
@@ -34,9 +34,9 @@
     [clj-time "0.12.0"] ; Date and time lib https://github.com/clj-time/clj-time
     [org.clojure/tools.cli "0.3.5"] ; Command-line parsing https://github.com/clojure/tools.cli
     [clj-jwt "0.1.1"] ; Library for JSON Web Token (JWT) https://github.com/liquidz/clj-jwt
-    [medley "0.8.2"] ; Utility functions https://github.com/weavejester/medley
+    [medley "0.8.3"] ; Utility functions https://github.com/weavejester/medley
     [com.stuartsierra/component "0.3.1"] ; Component Lifecycle
-    [amazonica "0.3.73"] ; A comprehensive Clojure client for the entire Amazon AWS api https://github.com/mcohen01/amazonica
+    [amazonica "0.3.74"] ; A comprehensive Clojure client for the entire Amazon AWS api https://github.com/mcohen01/amazonica
   ]
 
   ;; All profile plugins
@@ -56,7 +56,7 @@
         :open-company-auth-passphrase "this_is_a_qa_secret" ; JWT secret
       }
       :dependencies [
-        [midje "1.9.0-alpha3"] ; Example-based testing https://github.com/marick/Midje
+        [midje "1.9.0-alpha4"] ; Example-based testing https://github.com/marick/Midje
         [ring-mock "0.1.5"] ; Test Ring requests https://github.com/weavejester/ring-mock
         [philoskim/debux "0.2.1"] ; `dbg` macro around -> or let https://github.com/philoskim/debux
       ]
