5dc388cccfb5296497d5ce9297b70012d67e8e21
Sean Johnson <sean@snootymonkey.com>
2016-08-23 16:26:28 -0400

    Notes on slug change.

--- REPL_notes.clj
+++ REPL_notes.clj
@@ -109,6 +109,18 @@
       (r/update {:update {:body "<p>It's time to learn. That's it, really. Put it in the hands of people we admire and trust, and <b>LEARN FAST</b>.</p><p><img src=\"https://cdn.filestackcontent.com/ge9NSlJTP2AXfwl0nGvk\" data-height=\"370\" data-width=\"555\"><br></p><p><br></p>"}})
       (r/run c)))
 
+;; Provide a new slug for a company
+(with-open [c (apply r/connect conn2)]
+  (-> (r/table "companies")
+      (r/insert (assoc (company/get-company conn "old-slug") :slug "new-slug"))
+      (r/run c)))
+(with-open [c (apply r/connect conn2)]
+  (-> (r/table "sections")
+      (r/get-all ["old-slug"] {:index "company-slug"})
+      (r/update {:slug "new-slug"})
+      (r/run c)))
+(company/delete-company conn "old-slug")
+
 ;; for more RethinkDB help, see:
 ;; https://github.com/apa512/clj-rethinkdb
 ;; http://apa512.github.io/clj-rethinkdb/
