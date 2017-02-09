029f0ea34da3e0017b5a084369be56d8766816b6
Sean Johnson <sean@snootymonkey.com>
2017-02-09 10:34:09 -0500

    Cleanup code.

--- src/oc/storage/representations/topic.clj
+++ src/oc/storage/representations/topic.clj
@@ -11,8 +11,5 @@
 (defn topic-template-for-rendering
   "Add a create link to the provided topic template."
   [org-slug board-slug topic]
-  (println org-slug)
-  (println board-slug)
-  (println topic)
   (assoc topic :links [(hateoas/create-link (url org-slug board-slug topic) {:content-type mt/entry-media-type
                                                                              :accept mt/entry-media-type})]))
