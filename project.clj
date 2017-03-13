871677789268c0b4bc8eaeaaeda79eb62e37b7d3
Sean Johnson <sean@snootymonkey.com>
2017-03-13 07:04:16 -0400

    Update dependency.

--- project.clj
+++ project.clj
@@ -24,9 +24,8 @@
     [clj-http "3.4.1"] ; HTTP client https://github.com/dakrone/clj-http
     [medley "0.8.4"] ; Utility functions https://github.com/weavejester/medley
     [zprint "0.2.16"] ; Pretty-print clj and EDN https://github.com/kkinnear/zprint
-    [environ "1.1.0"] ; Environment settings from different sources https://github.com/weavejester/environ
     
-    [open-company/lib "0.6.13-a59242c"] ; Library for OC projects https://github.com/open-company/open-company-lib
+    [open-company/lib "0.6.14-22f40e5"] ; Library for OC projects https://github.com/open-company/open-company-lib
     ; In addition to common functions, brings in the following common dependencies used by this project:
     ; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
@@ -40,6 +39,7 @@
     ; Cheshire - JSON encoding / decoding https://github.com/dakrone/cheshire
     ; clj-jwt - A Clojure library for JSON Web Token(JWT) https://github.com/liquidz/clj-jwt
     ; clj-time - Date and time lib https://github.com/clj-time/clj-time
+    ; environ - Get environment settings from different sources https://github.com/weavejester/environ
   ]
 
   ;; All profile plugins
