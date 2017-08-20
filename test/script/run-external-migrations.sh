3a82b3625e09fb8f483c121890902e1f12212652
Sean Johnson <sean@snootymonkey.com>
2017-08-20 08:21:53 -0400

    Travis CI problem wasn't DB related. Tests fake the DB. Was due to a ns refactor. Fixed the tests.

--- test/script/run-external-migrations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Clone interactions repo
- cd ./opt/
- git clone https://github.com/open-company/open-company-interaction
- # Run the migrations
- cd open-company-interaction
-lein with-profile qa migrate-db
