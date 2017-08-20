2060ab712cf4e4d4560693611731f6cd00db0090
Sean Johnson <sean@snootymonkey.com>
2017-08-20 07:54:18 -0400

    Remove for wrong permissions.

--- test/script/merge-migrations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Clone interactions repo
- cd ./opt/
- git clone https://github.com/open-company/open-company-interaction
- # Copy migrations
- cp ./open-company-interaction/src/oc/interaction/db/migrations/* ../src/oc/storage/db/migrations/.
