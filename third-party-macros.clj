5d00ef053c05a4cd4173be37b7436fb7573571c6
Sean Johnson <sean@snootymonkey.com>
2017-11-06 09:07:41 -0500

    More specificity in Eastwood config.

--- third-party-macros.clj
+++ third-party-macros.clj
@@ -1,6 +1,6 @@
 (disable-warning
  {:linter :suspicious-expression
-  ;; specifically, those detected in function suspicious-macro-invocations
+  :for-macro 'clojure.core/and
   :if-inside-macroexpansion-of #{'defun.core/defun}
-  :within-depth 12
+  :within-depth 20
   :reason "defun creates calls to and with only 1 argument."})
