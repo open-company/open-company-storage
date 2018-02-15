0ddc640fadf3eaf906a05d2de255b7113dced050
Sean Johnson <sean@snootymonkey.com>
2018-02-15 18:05:16 -0500

    Emphasize that `(go)` is meant to be used at the REPL, not the old `(start)`.

--- src/dev.clj
+++ src/dev.clj
@@ -19,11 +19,12 @@
 (defn bind-conn! []
   (alter-var-root #'conn (constantly (pool/claim (get-in system [:db-pool :pool])))))
 
-(defn start []
+(defn- start⬆ []
   (alter-var-root #'system component/start))
 
 (defn stop []
-  (alter-var-root #'system (fn [s] (when s (component/stop s)))))
+  (alter-var-root #'system (fn [s] (when s (component/stop s))))
+  (println (str "\nWhen you're ready to start the system again, just type: (go)\n")))
 
 (defn go
   
@@ -31,7 +32,7 @@
   
   ([port]
   (init port)
-  (start)
+  (start⬆)
   (bind-conn!)
   (app/echo-config port)
   (println (str "Now serving storage from the REPL.\n"
