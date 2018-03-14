ac381664b99db5d72e36e2989c1bcbaa0f99b2a4
Sean Johnson <sean@snootymonkey.com>
2018-03-14 10:13:19 -0400

    Update props in the email share request.

--- src/oc/storage/async/email.clj
+++ src/oc/storage/async/email.clj
@@ -12,25 +12,37 @@
    :subject (schema/maybe schema/Str)
    :note (schema/maybe schema/Str)
    :reply-to (schema/maybe schema/Str)
+   :sharer-name (schema/maybe schema/Str)
    :org-slug lib-schema/NonBlankStr
    :org-name (schema/maybe schema/Str)
    :org-logo-url (schema/maybe schema/Str)
-   :headline (schema/maybe schema/Str)
+   :org-logo-width (schema/maybe schema/Int)
+   :org-logo-height (schema/maybe schema/Int)
+   :board-name (schema/maybe schema/Str)
+   :headline schema/Str
+   :body (schema/maybe schema/Str)
    :secure-uuid lib-schema/UniqueID
+   :publisher lib-schema/Author
    :published-at lib-schema/ISO8601
    :shared-at lib-schema/ISO8601})
 
-(defn ->trigger [org entry share-request user]
+(defn ->trigger [org board entry share-request user]
   {:type "share-entry"
    :to (vec (:to share-request))
    :subject (:subject share-request)
    :note (:note share-request)
    :reply-to (:email user)
+   :sharer-name (:name user)
    :org-slug (:slug org)
    :org-name (:name org)
    :org-logo-url (:logo-url org)
+   :org-logo-width (:logo-width org)
+   :org-logo-height (:logo-height org)
+   :board-name (:name board)
    :headline (:headline entry)
+   :body (:body entry)
    :secure-uuid (:secure-uuid entry)
+   :publisher (lib-schema/author-for-user (:publisher entry))
    :published-at (:published-at entry)
    :shared-at (:shared-at share-request)})
 
