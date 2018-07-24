42b7bb1f67abe1cf5ac99b30473a3b4136c942d6
Nathan Zorn <nathan.zorn@gmail.com>
2018-07-24 14:07:57 -0400

    receive webhooks from ziggeo

---
+++ src/oc/storage/api/videos.clj
@@ -0,0 +1,36 @@
+(ns oc.storage.api.videos
+  "Liberator API for ziggeo video resources."
+  (:require [taoensso.timbre :as timbre]
+            [compojure.core :as compojure :refer (POST)]
+            [liberator.core :refer (defresource by-method)]
+            [oc.lib.db.pool :as pool]
+            [oc.lib.api.common :as api-common]
+            [oc.storage.config :as config]))
+
+(defn- handle-video-webhook [conn ctx]
+  (timbre/debug ctx))
+
+;; ----- Resources - see: http://clojure-liberator.github.io/liberator/assets/img/decision-graph.svg
+
+;; A resource for operations on a zigge video
+(defresource video [conn]
+  (api-common/open-company-anonymous-resource config/passphrase)
+
+  :allowed-methods [:post]
+  ;; Media type client accepts
+  :available-media-types ["application/json"]
+  :handle-not-acceptable (api-common/only-accept 406 "application/json")
+
+  ;; Actions
+  :post! (fn [ctx] (handle-video-webhook conn ctx)))
+;; ----- Routes -----
+
+(defn routes [sys]
+  (let [db-pool (-> sys :db-pool :pool)]
+    (compojure/routes
+      ;; Secure UUID access
+      (POST "/ziggeo/videos"
+        []
+        (pool/with-pool [conn db-pool] 
+          (video conn))))))
+
