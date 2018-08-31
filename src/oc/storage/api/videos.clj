899f0f4d066612e72910d370d8cfe73355557919
Nathan Zorn <nathan.zorn@gmail.com>
2018-08-31 15:32:07 -0400

    handle auto share on video processing.

--- src/oc/storage/api/videos.clj
+++ src/oc/storage/api/videos.clj
@@ -5,11 +5,30 @@
             [liberator.core :refer (defresource by-method)]
             [oc.lib.db.pool :as pool]
             [oc.lib.api.common :as api-common]
+            [oc.storage.auth :as auth]
+            [oc.storage.api.entries :as entries-api]
             [oc.storage.async.notification :as notification]
             [oc.storage.resources.entry :as entry-res]
             [oc.storage.resources.org :as org-res]
             [oc.storage.resources.board :as board-res]
             [oc.storage.config :as config]))
+
+(defn- update-video-data
+  [conn org board video entry]
+  (if-not (= (:state_string video) "ERROR")
+    (let [user (auth/user-data (:user-id (first (:author entry))))
+          ctx {:user (assoc user :name (str (:first-name user) " "
+                                            (:last-name user)))
+               :existing-org org
+               :existing-board board
+               :existing-entry entry}
+          updated-entry (entry-res/update-video-data conn video entry)]
+      (when (= (:status updated-entry) "published")
+        (try
+          (entries-api/auto-share-on-publish conn ctx updated-entry)
+          (catch Exception e (timbre/error "caught exception: " (.getMessage e)))))
+      updated-entry)
+    (entry-res/error-video-data conn entry)))
 
 (defn- handle-video-webhook [conn ctx]
   (let [body (:data ctx)
@@ -19,16 +38,12 @@
                 (entry-res/get-entry-by-video conn token)
                 false)
         video-changed (:video-processed entry)]
-    (timbre/debug (:state_string video) video entry)
     (when entry
-      (let [error (= (:state_string video) "ERROR")
-            updated-result (if-not error
-                             (entry-res/update-video-data conn video entry)
-                             (entry-res/error-video-data conn entry))
-            ;; find org
+      (let [;; find org
             org (org-res/get-org conn (:org-uuid entry))
             ;; find board
-            board (board-res/get-board conn (:board-uuid entry))]
+            board (board-res/get-board conn (:board-uuid entry))
+            updated-result (update-video-data conn org board video entry)]
         (when (not= (:video-processed updated-result) video-changed)
           (notification/send-trigger!
            (notification/->trigger :update org board
