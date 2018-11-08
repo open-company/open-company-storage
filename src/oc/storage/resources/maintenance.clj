b4ed4cbd99f1f08dddd59fced98b0c9612a14db0
bago2k4 <iacopo@rc0.eu>
2018-11-08 14:02:39 +0100

    Better input params, default to dry run if no explicit parameter passed, better logs.

--- src/oc/storage/resources/maintenance.clj
+++ src/oc/storage/resources/maintenance.clj
@@ -7,15 +7,16 @@
             [oc.storage.resources.org :as org-res]
             [oc.storage.resources.common :as common]))
 
-(defn- update-entry [conn entry original-entry]
+(defn- update-entry [conn entry original-entry dry-run]
   (schema/validate common/Entry entry)
   (timbre/info "--> Updating entry from" (count (:shared original-entry)) "to" (count (:shared entry)))
-  (db-common/update-resource conn common/entry-table-name :uuid original-entry entry (db-common/current-timestamp)))
+  (when-not dry-run
+    (db-common/update-resource conn common/entry-table-name :uuid original-entry entry (db-common/current-timestamp))))
 
 (schema/defn ^:always-validate shared-limit-for-entry!
   "Given a RethinkDB connection, an entry map and a limit to apply to share cut the list of
    shared to the passed value keeping only the newest"
-  [conn :- lib-schema/Conn entry :- common/Entry limit]
+  [conn :- lib-schema/Conn entry :- common/Entry limit dry-run]
   (let [old-shared (:shared entry)
         grouped-shared (group-by :shared-at old-shared)
         unique-shared (vec (map first (vals grouped-shared)))
@@ -24,7 +25,7 @@
     (timbre/info "      Old shared" (count old-shared))
     (timbre/info "      Unique shared" (count unique-shared))
     (timbre/info "      Limited shared" (count limited-shared))
-    (update-entry conn (assoc entry :shared limited-shared) entry)
+    (update-entry conn (assoc entry :shared limited-shared) entry dry-run)
     (timbre/info "      Cut!")))
 
 (schema/defn ^:always-validate list-all-entries-by-org!
@@ -34,24 +35,35 @@
 (schema/defn ^:always-validate shared-limit-for-org!
   "Give a RethinkDB connection and an org-uuid, load all the published entries of the org
    and call the shared-limit-for-entry on all that are exceeding the shared limit."
-  [conn :- lib-schema/Conn org-uuid :- lib-schema/UniqueID limit]
-  (timbre/info "  Cut for org" org-uuid)
+  [conn :- lib-schema/Conn org-uuid :- lib-schema/UniqueID limit dry-run]
   (let [entries (list-all-entries-by-org! conn org-uuid)]
     (timbre/info "  Entries count:" (count entries))
     (for [entry entries]
       (do
         (timbre/info "    Checking entry" (:uuid entry) "shared:" (count (:shared entry)))
         (when (> (count (:shared entry)) limit)
-          (shared-limit-for-entry! conn entry limit))))))
+          (shared-limit-for-entry! conn entry limit dry-run))))))
 
 (defn shared-limit!
   "List all the present orgs, for each load all the entries that have more then 20 shared entries
    and cut them to the 20 more recent values."
-  [conn batch-length batch-offset & [limit]]
-  (let [limit (or limit 50)]
+  [conn {:keys [batch-length batch-offset limit dry-run]}]
+  (let [batch-length (or batch-length 20)
+        batch-offset (or batch-offset 0)
+        dry-run (if (nil? dry-run)
+                  true
+                  dry-run)
+        limit (or limit 50)]
     (timbre/info "Cut :shared lists for all entries to:" limit "(orgs from " (* batch-length batch-offset) "to" (* batch-length (inc batch-offset)) ")")
+    (when dry-run
+      (timbre/info "---- Dry-run not updating ----"))
     (let [orgs (org-res/list-orgs conn)
-          batches (vec (partition batch-length orgs))
-          batch (vec (get batches batch-offset))]
+          partition-length (min batch-length (count orgs))
+          batches (into [] (partition partition-length orgs))
+          batch (into [] (get batches batch-offset))]
+      (timbre/info "Total orgs:" (count orgs) " batch: " (inc batch-offset) "/" (count batches))
+      (timbre/info "Current batch size:" (count batch))
       (for [org batch]
-        (shared-limit-for-org! conn (:uuid org) limit)))))
+        (do
+          (timbre/info "Checking:" (:slug org))
+          (shared-limit-for-org! conn (:uuid org) limit dry-run))))))
