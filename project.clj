f272ebe2ee358335a4e858b8b81db385032d560b
Nathan Zorn <nathan.zorn@gmail.com>
2018-12-11 11:32:43 -0500

    version bump

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
     ;; Pretty-print clj and EDN https://github.com/kkinnear/zprint
     [zprint "0.4.13"]
     ;; Library for OC projects https://github.com/open-company/open-company-lib
-    [open-company/lib "0.16.28alpha3"]
+    [open-company/lib "0.16.28"]
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; httpkit - HTTP client/server http://www.http-kit.org/
     ;; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
