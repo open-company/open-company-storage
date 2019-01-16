e795b8cb0481d528ed5831db29e3be0bc32fc2ec
Sean Johnson <sean@snootymonkey.com>
2019-01-16 13:36:12 -0500

    Comment.

--- src/oc/storage/representations/media_types.clj
+++ src/oc/storage/representations/media_types.clj
@@ -27,4 +27,5 @@
 (def reaction-media-type "application/vnd.open-company.reaction.v1+json")
 (def reaction-collection-media-type "application/vnd.collection+vnd.open-company.reaction+json;version=1")
 
+;; Reminder media types
 (def reminders-list-media-type "application/vnd.collection+vnd.open-company.reminder+json;version=1")
