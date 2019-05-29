22d8382580cefb3e2792f5523dc59d617af01601
Sean Johnson <sean@snootymonkey.com>
2019-05-29 09:35:25 -0400

    Update lib version.

--- project.clj
+++ project.clj
@@ -38,7 +38,7 @@
     [zprint "0.4.15"]
     
     ;; Library for OC projects https://github.com/open-company/open-company-lib
-    [open-company/lib "0.17.9-alpha1"]
+    [open-company/lib "0.17.9"]
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; httpkit - HTTP client/server http://www.http-kit.org/
     ;; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
