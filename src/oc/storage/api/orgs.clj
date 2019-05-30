1dde46d3e8c5e5196a75670b3985e1e1b70fd463
bago2k4 <iacopo@rc0.eu>
2019-05-30 13:31:01 +0200

    Show drafts board to viewers with at least one author access board.

--- src/oc/storage/api/orgs.clj
+++ src/oc/storage/api/orgs.clj
@@ -256,7 +256,14 @@
                              boards (board-res/list-boards-by-org conn org-id [:created-at :updated-at :authors :viewers :access])
                              board-access (map #(board-with-access-level org % user) boards)
                              allowed-boards (filter :access-level board-access)
-                             show-draft-board? (and (seq user-id) (access/allow-authors conn slug (:user ctx)))
+                             author-access-boards (filter #(= (:access-level %) :author) board-access)
+                                               ;; Add the draft board
+                             show-draft-board? (and ;; if user is logged in and
+                                                    (seq user-id)
+                                                    ;; or is an author of the org
+                                                    (or (access/allow-authors conn slug (:user ctx))
+                                                        ;; or has at least one board with author access
+                                                        (pos? (count author-access-boards))))
                              draft-entry-count (if show-draft-board? (entry-res/list-entries-by-org-author conn org-id user-id :draft {:count true}) 0)
                              must-see-count (entry-res/list-entries-by-org conn org-id :asc (db-common/current-timestamp) :before (map :uuid allowed-boards) {:must-see true :count true})
                              full-boards (if show-draft-board?
