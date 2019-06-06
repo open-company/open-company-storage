2968b0fe334a80c272dc5493ecd9827c62da016e
bago2k4 <iacopo@rc0.eu>
2019-06-06 21:56:03 +0200

    Add paginated-entries-by-board function to entry resource used for the new board pagination.

--- src/oc/storage/resources/entry.clj
+++ src/oc/storage/resources/entry.clj
@@ -382,7 +382,7 @@
 
 (schema/defn ^:always-validate list-entries-by-org
   "
-  Given the UUID of the org, an order, one of `:asc` or `:desc`, a start date as an ISO8601 timestamp, 
+  Given the UUID of the org, an order, one of `:asc` or `:desc`, a start date as an ISO8601 timestamp,
   and a direction, one of `:before` or `:after`, return the published entries for the org with any interactions.
   "
   [conn org-uuid :- lib-schema/UniqueID order start :- lib-schema/ISO8601 direction allowed-boards :- [lib-schema/UniqueID] {:keys [must-see count] :or {must-see false count false}}]
@@ -402,9 +402,26 @@
       ["uuid" "headline" "body" "reaction" "author"
        "published-at" "created-at" "updated-at"] {:count count})))
 
+
+(schema/defn ^:always-validate paginated-entries-by-board
+  "
+  Given the UUID of the org, an order, one of `:asc` or `:desc`, a start date as an ISO8601 timestamp,
+  and a direction, one of `:before` or `:after`, return the published entries for the org with any interactions.
+  "
+  [conn board-uuid :- lib-schema/UniqueID order start :- lib-schema/ISO8601 direction {:keys [count] :or {count false}}]
+  {:pre [(db-common/conn? conn)
+          (#{:desc :asc} order)
+          (#{:before :after} direction)]}
+  (db-common/read-resources-and-relations conn table-name
+    :status-board-uuid [[:published board-uuid]]
+    "published-at" order start direction config/default-activity-limit
+    :interactions common/interaction-table-name :uuid :resource-uuid
+    ["uuid" "headline" "body" "reaction" "author"
+     "published-at" "created-at" "updated-at"] {:count count}))
+
 (schema/defn ^:always-validate list-entries-by-org-author
   "
-  Given the UUID of the org, an order, one of `:asc` or `:desc`, a start date as an ISO8601 timestamp, 
+  Given the UUID of the org, an order, one of `:asc` or `:desc`, a start date as an ISO8601 timestamp,
   and a direction, one of `:before` or `:after`, and an optional status of `:draft` or `:published` (the default)
   return the entries by the author with any interactions.
   "
