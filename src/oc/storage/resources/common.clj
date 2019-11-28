86c5594e63c250960c716a89494fc0361e9251a2
bago2k4 <iacopo@rc0.eu>
2019-11-28 17:01:14 +0100

    Add UserVisibility schema and attach it to the entry schema.

--- src/oc/storage/resources/common.clj
+++ src/oc/storage/resources/common.clj
@@ -96,6 +96,11 @@
 
 (def Status (schema/pred #(#{:draft :published} (keyword %))))
 
+(def UserVisibility
+  "A user-visibility item."
+  {(schema/optional-key :dismiss-at) lib-schema/ISO8601
+   (schema/optional-key :follow) schema/Bool})
+
 (def FollowUp
   "A follow-up item."
   {
@@ -146,7 +151,9 @@
   :updated-at lib-schema/ISO8601
 
   ;; FollowUps
-  (schema/optional-key :follow-ups) [FollowUp]})
+  (schema/optional-key :follow-ups) [FollowUp]
+  (schema/optional-key :user-visibility) (schema/maybe {schema/Keyword UserVisibility})
+})
 
 (def NewBoard
   "A new board for creation, can have new or existing entries already."
