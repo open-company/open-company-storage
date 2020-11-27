da785e904997e58f782745f5debdbdb2fa9e6e7d
bago2k4 <iacopo@rc0.eu>
2020-11-27 21:12:53 +0100

    Update lib to fix handle unprocessable entity.

--- project.clj
+++ project.clj
@@ -49,7 +49,7 @@
     ;; ***************** (JWT schema changes, more info here: *****************
     ;; ******* https://github.com/open-company/open-company-lib/pull/82) ******
     ;; ************************************************************************
-    [open-company/lib "0.17.29-alpha60"]
+    [open-company/lib "0.17.29-alpha61"]
     ;; ************************************************************************
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; httpkit - HTTP client/server http://www.http-kit.org/
