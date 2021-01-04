ec4a52be362b242f3ac3eaf072b893afcf357645
bago2k4 <iacopo@rc0.eu>
2021-01-04 14:03:14 +0100

    Fix parse of env vars to integer.

--- src/oc/storage/config.clj
+++ src/oc/storage/config.clj
@@ -117,5 +117,5 @@
 
 (defonce unread-days-limit (or (env :unread-days-limit) 30))
 
-(defonce unseen-cap-days (Integer/parseInt (or (env :unseen-cap-days) (* 365 50))))
-(defonce pins-sort-pivot-days (Integer/parseInt (or (env :pins-sort-pivot-days) (* 365 100))))
+(defonce unseen-cap-days (Integer/parseInt (or (env :unseen-cap-days) (str (* 365 50)))))
+(defonce pins-sort-pivot-days (Integer/parseInt (or (env :pins-sort-pivot-days) (str (* 365 100)))))
